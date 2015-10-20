f20316ca364d325596fbe3395fe63c91450014cb
Yu Chen Hou <houyuchen66@gmail.com>
2015-10-20 14:58:28 -0400

    Update build.js

--- build.js
+++ build.js
@@ -3,5 +3,5 @@ var handlebars = require("handlebars");
 
 var template = handlebars.compile(fs.readFileSync("template.html", "utf8"));
 var people = JSON.parse(fs.readFileSync("people.json", "utf8"));
-fs.writeFileSync("index.html", "<!-- This is a generated file! Please edit template.html -->");
+fs.writeFileSync("index.html", "<!-- This is a generated file! Please edit template.html -->\n");
 fs.appendFileSync("index.html", template(people));
