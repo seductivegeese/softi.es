aa3e9a799ad4a10c3ccfdaffe27d520029e5eba5
Dave Pagurek <dave@davepagurek.com>
2015-10-26 18:19:52 -0400

    Ignore index.html

--- build.js
+++ build.js
@@ -3,5 +3,4 @@ var handlebars = require("handlebars");
 
 var template = handlebars.compile(fs.readFileSync("template.html", "utf8"));
 var people = JSON.parse(fs.readFileSync("people.json", "utf8"));
-fs.writeFileSync("index.html", "<!-- This is a generated file! Please edit template.html -->\n");
-fs.appendFileSync("index.html", template(people));
+fs.writeFileSync("index.html", template(people));
